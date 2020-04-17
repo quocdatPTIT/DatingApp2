ade3639c950af1b0cb2e6fcb566da52fbe995bc9
quocdatPTIT <datsieunhan16@gmail.com>
2020-04-17 17:00:37 +0700

    add repository cho method inbox, outbox

--- DatingApp.API/Models/Message.cs
+++ DatingApp.API/Models/Message.cs
@@ -13,6 +13,7 @@ namespace DatingApp.API.Models
         public bool IsRead { get; set; }
         public DateTime? DateRead { get; set; }
         public bool SenderDeleted { get; set; }
+        public DateTime MessageSent { get; set; }
         public bool RecipientDeleted { get; set; }
     }
 }
